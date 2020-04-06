2ce7643cd721c313a710ef16f21d4135c67f014c
tolewicz <59777198+tolewicz@users.noreply.github.com>
2020-04-06 09:55:19 -0700

    added comments

--- plots.js
+++ plots.js
@@ -12,6 +12,7 @@ function init() {
       });
   })};
 
+  //state 0 when the page is open
   buildMetadata(940);
   buildCharts(940);
 
@@ -36,38 +37,48 @@ function buildMetadata(sample) {
     });
       })};
 
+// function for building the chart
 function buildCharts(sample){ 
+    
     
        d3.json("samples.json").then((data) => {
         
+        //prase data from json file
         var samples = data.samples;
         var filterSamples = samples.filter(kupaObj => kupaObj.id == sample);
         
+        //prasing the arrays otu_ids, sample_values, otu_labels
         otuIds = filterSamples[0].otu_ids
         otuValues = filterSamples[0].sample_values
         otuLabels = filterSamples[0].otu_labels
- 
+        
+        //creating empty lists that will be filled with items from otu_ids, sample_values, otu_labels
+        //[(key) name: otu_itd, (value) count: sample_values, (value) labels: otu_labels]   
         var bacteriaList = []
         var bacteriaLabel = []
-
+        
+        // forEach function used to extract items from otu_ids, sample_values, otu_labels and put them into empty lists 
         otuIds.forEach((item,index) => {
         bacteriaList[index] = {name: item, count: otuValues[index], label: otuLabels[index] }
         })
         
         bacteriaListCopy = bacteriaList.slice()
-
+        
+        //checkup console print
         console.log(`unsorted list:`)
         console.log(bacteriaList[0])
         console.log(`unsorted bacteria set:`)
         console.log(bacteriaLabel[0])
-
+        
+        //sorting the data in descending order
         var sortedBacterialList = bacteriaListCopy.sort(function(a,b) {
           return parseFloat(b.count) - parseFloat(a.count)
         });
-
+        
+        //selecting top 10 items
         var sortedBacterialList = sortedBacterialList.slice(0,10)
         
-        //bar chart
+        //creating bar chart
         var trace = {
           y: sortedBacterialList.map(row => `OTU  ${row.name}`).reverse(),
           x: sortedBacterialList.map(row => `${row.count}`).reverse(),
@@ -101,11 +112,12 @@ function buildCharts(sample){
 
       Plotly.newPlot("Bar_Chart", sortedBacterialList,layout);
 
-      //buble cahrt
+      //creating buble cahrt
       
       var xvalues = []
       var yvalues = []
       var labels = []
+      
       bacteriaListCopy.forEach(function(row){
         xvalues.push(row.name)
         yvalues.push(row.count)
